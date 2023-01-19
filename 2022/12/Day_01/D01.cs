ac4ef4d1c2ef3a00b667190f16317b7bc8c4d6a6
phobo3s <bberkkilinc@gmail.com>
2023-01-19 17:46:35 +0300

    2023-01-19

---
+++ 2022/12/Day_01/D01.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Day16
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            GetValveData();
+            
+        }
+
+        
+        private static void GetValveData()
+        {
+            Dictionary<string, string>  valveData = new Dictionary<string, string>();
+            string fileText = File.ReadAllText("..\\..\\Resources\\ProblemData.txt");
+            var valveDataText = fileText.Split('\n').Select(line => line.Split(';'));
+            foreach (var valve in valveDataText)
+            {
+               
+            };
+        }
+
+    }
+}
