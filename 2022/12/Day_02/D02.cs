2ddbbcb98bbd319accade4d853e4861858439ace
phobo3s <bberkkilinc@gmail.com>
2023-01-21 16:42:44 +0300

    2023-01-21

---
+++ 2022/12/Day_02/D02.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Day_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fileText = File.ReadAllText("..\\..\\Resources\\ProblemData.txt");
+            string[] DataText = fileText.Split(new string[] { "\r\n\r\n" }, StringSplitOptions.None);
+        }
+    }
+}
