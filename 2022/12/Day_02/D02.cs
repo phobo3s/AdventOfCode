64cfde65e2a17746cabe9e70fa78d985f911c46a
phobo3s <bberkkilinc@gmail.com>
2023-01-24 17:19:48 +0300

    2023-01-24

--- 2022/12/Day_02/D02.cs
+++ 2022/12/Day_02/D02.cs
@@ -9,10 +9,44 @@ namespace Day_02
 {
     class Program
     {
+        private enum player1 {A=3,B=2,C=1};
+        private enum player2 {X=3,Y=2,Z=1};
         static void Main(string[] args)
         {
             string fileText = File.ReadAllText("..\\..\\Resources\\ProblemData.txt");
-            string[] DataText = fileText.Split(new string[] { "\r\n\r\n" }, StringSplitOptions.None);
+            string[] gameData = fileText.Split(new string[] { "\r\n" }, StringSplitOptions.None);
+            int totalPoint = 0;
+            
+            //resolve
+            foreach (string aGame in gameData)
+            {
+                string[] playerChoices = aGame.Split(' ');
+                int player1Choice = (int)Enum.Parse(typeof(player1), playerChoices[0]);
+                int player2Choice = (int)Enum.Parse(typeof(player2), playerChoices[1]);
+
+
+                    if ((Math.Abs(player1Choice-player2Choice) % 2) == 0) //is the difference is even
+                    {
+                        if (player2Choice == Math.Min(player1Choice,player2Choice))
+                        {
+                            totalPoint = totalPoint + 6;
+                        }
+                        else
+                        {
+                            if (player2Choice == player1Choice)
+                            {
+                                totalPoint = totalPoint + 3;
+                            }
+                        }
+                    }else //or odd    
+                    {
+                        if (player2Choice == Math.Max(player1Choice,player2Choice))
+                        {
+                            totalPoint = totalPoint + 6;
+                        }
+                    }
+                    totalPoint = totalPoint + player2Choice;
+            }
         }
     }
 }
