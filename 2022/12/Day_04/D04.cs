b3186d512b7eadf4e8643768a7da3860229ceeaf
phobo3s <bberkkilinc@gmail.com>
2023-01-26 14:07:04 +0300

    2023-01-26

---
+++ 2022/12/Day_04/D04.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Day_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fileText = File.ReadAllText("..\\..\\Resources\\ProblemData.txt");
+            string[] teamData = fileText.Split(new string[] { "\r\n" }, StringSplitOptions.None);
+            //resolve Part-2
+            int errorCount = 0;
+            foreach (string aTeamPair in teamData)
+            {
+                string[] teams = aTeamPair.Split(',');
+                int xi = int.Parse(teams[0].Split('-')[0]);
+                int xj = int.Parse(teams[0].Split('-')[1]);
+                int yi = int.Parse(teams[1].Split('-')[0]);
+                int yj = int.Parse(teams[1].Split('-')[1]);
+                if ((xj<yi)||(xi>yj))
+                {
+                    //
+                }
+                else
+                {
+                    errorCount++;
+                }
+            }
+
+            //resolve Part-1
+            errorCount = 0;
+            foreach (string aTeamPair in teamData)
+            {
+                string[] teams = aTeamPair.Split(',');
+                int xi = int.Parse(teams[0].Split('-')[0]);
+                int xj = int.Parse(teams[0].Split('-')[1]);
+                int yi = int.Parse(teams[1].Split('-')[0]);
+                int yj = int.Parse(teams[1].Split('-')[1]);
+                if ((xi<=yi && xj>=yj)||(xi>=yi && xj<=yj))
+                {
+                    errorCount++;
+                }
+            }
+            Console.WriteLine("the end");
+
+        }
+    }
+}
---
+++ 2022/12/Day_04/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("Day_04")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Day_04")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("3e5c55b4-28b7-4bdc-8215-9ca9e16dc819")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+// Tüm değerleri belirtebilir veya varsayılan Derleme ve Düzeltme Numaralarını kullanmak için
+// aşağıda gösterildiği gibi '*' kullanabilirsiniz:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
